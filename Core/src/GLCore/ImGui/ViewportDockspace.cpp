1c83798f5d6ae706bb94ece9768ddfbb0d1ab85c
Spice <kf.shared.ptr@gmail.com>
2022-02-10 22:11:28 -0600

    Non-transparent viewport framebuffer.

--- Core/src/GLCore/ImGui/ViewportDockspace.cpp
+++ Core/src/GLCore/ImGui/ViewportDockspace.cpp
@@ -32,7 +32,7 @@ namespace ImGuiExt {
 
 		glCreateTextures(GL_TEXTURE_2D, 1, &m_ColorAttachment0);
 		glBindTexture(GL_TEXTURE_2D, m_ColorAttachment0);
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, m_Width, m_Height, 0, GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB8, m_Width, m_Height, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
